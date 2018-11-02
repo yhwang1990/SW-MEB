8b9b2efb3adba982d6dee185bf7b6188622a06c6
yhwang1990 <wangyanhao990@gmail.com>
2018-11-02 15:48:40 +0800

    SW-MEB v0.01

--- src/generator/RandomGenerator.java
+++ src/generator/RandomGenerator.java
@@ -86,4 +86,32 @@ public class RandomGenerator {
 			e.printStackTrace();
 		}
 	}
+	
+	public static void genNormal(int n, int d, long seed) {
+		Random rand = new Random(seed);
+		try {
+			FileWriter fw = new FileWriter("normal-" + n + "-" + d + ".txt");
+			fw.write(n + " " + d + "\n");
+			for (int i = 0; i < n; i++) {
+				StringBuilder builder = new StringBuilder();
+				for (int j = 0; j < d; j++) {
+					double num = Double.MAX_VALUE;
+					while (num >= 10.0 || num <= -10.0) {
+						num = rand.nextGaussian();
+					}
+					builder.append(FORMAT.format(num)).append(" ");
+				}
+				builder.deleteCharAt(builder.length() - 1).append("\n");
+				fw.write(builder.toString());
+
+				if (i > 0 && i % 100000 == 0) {
+					System.out.println(i);
+					fw.flush();
+				}
+			}
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
