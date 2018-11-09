d3e79c12adf3d06150c9bb5c9713ad6b3f232620
yhwang1990 <wangyanhao990@gmail.com>
2018-11-09 16:37:04 +0800

    SW-MEB v0.08

--- src/generator/RandomGenerator.java
+++ src/generator/RandomGenerator.java
@@ -7,23 +7,23 @@ import java.util.Random;
 
 public class RandomGenerator {
 
-	static DecimalFormat FORMAT = new DecimalFormat("#.###");
+	static DecimalFormat FORMAT = new DecimalFormat("#.##");
 
 	public static void genUniform(int n, int d) {
 		Random rand = new Random(0l);
 		try {
 			FileWriter fw = new FileWriter("uniform-" + n + "-" + d + ".txt");
-			fw.write(n + " " + d + "\n");
+//			fw.write(n + " " + d + "\n");
 			for (int i = 0; i < n; i++) {
 				StringBuilder builder = new StringBuilder();
 				for (int j = 0; j < d; j++) {
-					double num = rand.nextDouble();
+					double num = rand.nextDouble() * 10.0;
 					builder.append(FORMAT.format(num)).append(" ");
 				}
 				builder.deleteCharAt(builder.length() - 1).append("\n");
 				fw.write(builder.toString());
 
-				if (i > 0 && i % 100000 == 0) {
+				if (i > 0 && i % 10000 == 0) {
 					System.out.println(i);
 					fw.flush();
 				}
@@ -38,17 +38,17 @@ public class RandomGenerator {
 		Random rand = new Random(seed);
 		try {
 			FileWriter fw = new FileWriter("uniform-" + n + "-" + d + ".txt");
-			fw.write(n + " " + d + "\n");
+//			fw.write(n + " " + d + "\n");
 			for (int i = 0; i < n; i++) {
 				StringBuilder builder = new StringBuilder();
 				for (int j = 0; j < d; j++) {
-					double num = rand.nextDouble();
+					double num = rand.nextDouble() * 10.0;
 					builder.append(FORMAT.format(num)).append(" ");
 				}
 				builder.deleteCharAt(builder.length() - 1).append("\n");
 				fw.write(builder.toString());
 
-				if (i > 0 && i % 100000 == 0) {
+				if (i > 0 && i % 10000 == 0) {
 					System.out.println(i);
 					fw.flush();
 				}
@@ -63,20 +63,21 @@ public class RandomGenerator {
 		Random rand = new Random(0l);
 		try {
 			FileWriter fw = new FileWriter("normal-" + n + "-" + d + ".txt");
-			fw.write(n + " " + d + "\n");
+//			fw.write(n + " " + d + "\n");
 			for (int i = 0; i < n; i++) {
 				StringBuilder builder = new StringBuilder();
 				for (int j = 0; j < d; j++) {
 					double num = Double.MAX_VALUE;
-					while (num >= 10.0 || num <= -10.0) {
+					while (num >= 5.0 || num <= -5.0) {
 						num = rand.nextGaussian();
 					}
+					num += 5.0;
 					builder.append(FORMAT.format(num)).append(" ");
 				}
 				builder.deleteCharAt(builder.length() - 1).append("\n");
 				fw.write(builder.toString());
 
-				if (i > 0 && i % 100000 == 0) {
+				if (i > 0 && i % 10000 == 0) {
 					System.out.println(i);
 					fw.flush();
 				}
@@ -91,20 +92,21 @@ public class RandomGenerator {
 		Random rand = new Random(seed);
 		try {
 			FileWriter fw = new FileWriter("normal-" + n + "-" + d + ".txt");
-			fw.write(n + " " + d + "\n");
+//			fw.write(n + " " + d + "\n");
 			for (int i = 0; i < n; i++) {
 				StringBuilder builder = new StringBuilder();
 				for (int j = 0; j < d; j++) {
 					double num = Double.MAX_VALUE;
-					while (num >= 10.0 || num <= -10.0) {
+					while (num >= 5.0 || num <= -5.0) {
 						num = rand.nextGaussian();
 					}
+					num += 5.0;
 					builder.append(FORMAT.format(num)).append(" ");
 				}
 				builder.deleteCharAt(builder.length() - 1).append("\n");
 				fw.write(builder.toString());
 
-				if (i > 0 && i % 100000 == 0) {
+				if (i > 0 && i % 10000 == 0) {
 					System.out.println(i);
 					fw.flush();
 				}
--- src/main/Generator.java
+++ src/main/Generator.java
@@ -5,8 +5,14 @@ import generator.RandomGenerator;
 public class Generator {
 
 	public static void main(String[] args) {
-		RandomGenerator.genUniform(200000, 100);
-		RandomGenerator.genNormal(200000, 100);
+		RandomGenerator.genUniform(2000000, 100);
+		RandomGenerator.genNormal(2000000, 100);
+		
+		RandomGenerator.genUniform(2000000, 1000);
+		RandomGenerator.genNormal(2000000, 1000);
+		
+		RandomGenerator.genUniform(200000, 10000);
+		RandomGenerator.genNormal(200000, 10000);
 	}
 
 }
